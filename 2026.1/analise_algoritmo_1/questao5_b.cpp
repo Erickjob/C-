90ac7a5d3576e2a12d8e17866b19b12b430a6295
erickjob <erickjob@gmail.com>
2026-04-19 17:39:43 -0300

    add

--- 2026.1/analise_algoritmo_1/questao5_b.cpp
+++ 2026.1/analise_algoritmo_1/questao5_b.cpp
@@ -4,50 +4,43 @@
 
 bool prime(long long n)
 {
-   if (n == 1)
-        std::cout << "falso" << std::endl;
-    
-    if(n == 2)
-        std::cout << "verdadeiro" << std::endl;
-    
+    if (n == 1)
+        return false;
+
+    if (n == 2)
+        return true;
+
     if (n % 2 == 0)
-        std::cout << "falso" << std::endl;
-    
+        return false;
+
     int quantidade_divisores = 1;
 
-    for (int i = 3; i < n/2; i++ ){
-        if (n % i == 0){
+    for (int i = 3; i < n / 2; i++)
+    {
+        if (n % i == 0)
+        {
             quantidade_divisores++;
         }
+
         return quantidade_divisores == 1;
+    }
 }
-int main(){
-   std::ifstream entrada("entrada.txt");
+
+int main()
+{
+    std::ifstream entrada("entrada.txt");
 
     long long numero;
 
     while (entrada >> numero) // lê vários valores
     {
-    auto beg = std::chrono::high_resolution_clock::now();
-
-    
-    auto end = std::chrono::high_resolution_clock::now();
-    
-    }
-    if(quantidade_divisores > 1){
-        std::cout << "falso, quantidade de divisores " << quantidade_divisores << " > 1" << std::endl;
-    }else{
-
-        
-     std::cout << "verdadeiro " << quantidade_divisores <<  std::endl;
-       // Fim do cronômetro
-        
-    auto dur = end - beg;
+        auto beg = std::chrono::high_resolution_clock::now();
+        bool p = prime(numero);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto dur = end - beg;
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(dur);
 
-        std::cerr << numero << " Processing time: "
-                  << duration.count() << " microseconds(s)" << std::endl;
+        std::cout << duration.count() << "\n";
     }
     return 0;
-    
 }
