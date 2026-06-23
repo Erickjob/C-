db6f9f33ea61ce633bab9448d40bfb6b55f51e72
Erickjob <163067063+Erickjob@users.noreply.github.com>
2026-06-23 19:37:40 +0000

    Organização das pastas com adição do _ onde ficava os espaços, como também adição criação da pasta lista ligada

---
+++ 2026.1/lista_lig/q1.cpp
@@ -0,0 +1,131 @@
+class ll_int
+{
+private:
+    struct ll_int_node
+    {
+        int value;
+        struct ll_int_node *next;
+    };
+    ll_int_node *first, *last;
+
+public:
+    ll_int()
+    {
+        this->first = nullptr;
+        this->last = nullptr;
+    }
+    void push_front(int value)
+    {
+        ll_int_node *new_node = new ll_int_node;
+        new_node->value = value;
+        new_node->next = this->first;
+        if (this->first == nullptr)
+            this->last = new_node;
+        this->first = new_node;
+    }
+    void push_back(int value)
+    {
+        ll_int_node *new_node = new ll_int_node;
+        new_node->value = value;
+        new_node->next = nullptr;
+        if (this->first == nullptr)
+            this->first = new_node;
+        else
+            this->last->next = new_node;
+        this->last = new_node;
+    }
+    void pop_front()
+    {
+        if (this->first == nullptr)
+            return;
+        ll_int_node *to_remove = this->first;
+        if (this->last == this->first)
+            this->last = nullptr;
+        this->first = this->first->next;
+        delete to_remove;
+    }
+    void pop_back()
+    {
+        if (this->last == nullptr)
+            return;
+        if (this->first == this->last)
+        {
+            delete this->first;
+            this->first = nullptr;
+            this->last = nullptr;
+        }
+        ll_int_node *current = this->first;
+        while (current->next != this->last)
+            current = current->next;
+        current->next = nullptr;
+        delete this->last;
+        this->last = current;
+    }
+    int front()
+    {
+        if (this->first != nullptr)
+            return this->first->value;
+        else
+            return -1;
+    }
+    int back()
+    {
+        if (this->last != nullptr)
+            return this->last->value;
+        else
+            return -1;
+    }
+    int sum()
+    {
+        int ans = 0;
+        ll_int_node *current = this->first;
+        while (current != 0)
+        {
+            ans += current->value;
+            current = current->next;
+        }
+        return ans;
+    }
+
+    int size(){                                  //O(n)
+        if (this->first == nullptr)
+            return -1;
+        if (this->first == this->last)
+            return 0;
+        int count = 1;
+        ll_int_node *current = this->first;
+        while (current != nullptr){
+            current = current->next;
+            count++;
+        }
+        return count;
+    }
+
+    int get_at(int index){                        //O(n)
+        ll_int_node *current = this-> first;
+        int indice=0;
+        while (indice != index){
+            current = current->next;
+        }
+        int value = current->value; 
+        return value;
+    }
+
+    int set_at(int indice, int value){           //O(n)
+        ll_int_node *current = this->first;
+        for (int i=0; i < indice; i++){
+            current = current->next;
+        }
+        current->value = value;
+    }
+
+    ~ll_int(){                                  //O(n²)
+        ll_int_node *current, *to_remove;
+        current = this -> first;
+        while(current != 0){
+            to_remove = current;
+            current = current->next;
+            delete[] current;
+        }
+    }
+};
