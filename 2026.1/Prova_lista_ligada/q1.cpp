bde1bc02aff502d93e95e6eaba42d1d979ebe3fc
erickjob <erickjob@gmail.com>
2026-07-20 22:31:14 -0300

    questão 1 finalizada

--- 2026.1/Prova_lista_ligada/q1.cpp
+++ 2026.1/Prova_lista_ligada/q1.cpp
@@ -11,23 +11,24 @@ private:
 
 public:
 
-    //Construtor
+        //Construtor
     single_list_int(){
         this-> head = nullptr;
         this-> tail = nullptr;
         this-> size_= 0;
     }
 
-    //Destrutor
+        //Destrutor
     ~single_list_int(){
         node *current = this->head;
         while (current != nullptr){
-            node *remove = current;
+            node *to_remove = current;
             current = current->next;
-            delete remove;
+            delete to_remove;
         }
     }
 
+        //Insere no início
     void push_front(int value){
         node *new_node = new node;
         new_node->value = value;
@@ -38,8 +39,37 @@ public:
             this->size_ ++;
         }
     }
+        //Insere no final
     void push_back(int value){
-        
+        node *new_node = new node;
+        new_node-> value = value;
+        new_node->next = 0;
+        if (this->head == 0)
+            this->head = new_node;
+        else
+            this->tail->next=new_node;
+            this->tail=new_node;
     }
 
+    bool remove (int value){
+        if (this->head == nullptr){return false;}
+
+        node *current = this->head;
+        while (current->next != 0 && current->next-> value != value){
+            current = current->next;
+        }
+        if (current->next == 0){
+            return false;
+        }
+        node *to_remove = current->next;
+        current->next = to_remove->next;
+
+        if (to_remove == this->tail){
+            this->tail = current;
+        }
+        delete to_remove;
+        return true;
+        
+
+    }
 };
